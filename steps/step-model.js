3b34ca691c87b15d8d91091aaabb063a8d572a6f
erozonachi <erozonachi.james@gmail.com>
2019-07-17 18:07:22 +0100

    Add find-by-id function to the step-model module

--- steps/step-model.js
+++ steps/step-model.js
@@ -4,4 +4,10 @@ module.exports = {
   find: function() {
     return db('steps');
   },
+
+  findById: function(id) {
+    return db('steps')
+      .where({ id })
+      .then(data => (data.length > 0 ? data[0] : null));
+  },
 };
